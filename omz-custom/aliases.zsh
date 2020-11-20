2e324e34a6dbd2a5b0863db30d69ff7efffabc68
Sam Fritton <Samuel.Fritton@target.com>
2020-11-20 13:42:49 -0600

    adding omz

---
+++ omz-custom/aliases.zsh
@@ -0,0 +1,5 @@
+alias reload="source ~/.zshrc" # read the Z shell file/ refreshes the terminal
+alias rmdir="rm -rf"
+alias cpdir="cp -r"
+alias config="vim $HOME/.my-config" # open config folder
+alias kill-docker="docker kill $(docker ps -q)" # kills all docker containers
